638327e48e10878562c73b9a50507851e23a3afb
Stéphane Bullier <sbullier@gmail.com>
2022-07-31 16:02:24 +0200

    Create User controller

---
+++ src/controller/user-controller.js
@@ -0,0 +1,14 @@
+const User = require('../model/user-model');
+
+const createUser = async (req, res) => {};
+
+const getUsers = async (req, res) => {};
+
+const editUser = async (req, res) => {};
+
+const deleteUser = async (req, res) => {};
+
+exports.createUser = createUser;
+exports.getUsers = getUsers;
+exports.editUser = editUser;
+exports.deleteUser = deleteUser;
