8d8d87a03cd9e68009b1ca325e177df0b321732b
Stéphane Bullier <sbullier@gmail.com>
2022-07-31 15:42:55 +0200

    Create router for Car

---
+++ src/router/car-router.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = Router();
+const {
+  createCar,
+  getCars,
+  editCar,
+  deleteCar,
+} = require('../controller/car-controller');
+
+router.post('/voitures/ajouter', createCar);
+router.get('/voitures', getCars);
+router.patch('/voitures/editer/:cid', editCar);
+router.delete('/voitures/:cid', deleteCar);
