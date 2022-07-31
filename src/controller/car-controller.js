f98a234983cf60b3e2bd94b0251ff8d595e87971
Stéphane Bullier <sbullier@gmail.com>
2022-07-31 15:42:36 +0200

    Create controller starter for Car

---
+++ src/controller/car-controller.js
@@ -0,0 +1,14 @@
+const Car = require('../model/car-model');
+
+const createCar = async (req, res) => {};
+
+const getCars = async (req, res) => {};
+
+const editCar = async (req, res) => {};
+
+const deleteCar = async (req, res) => {};
+
+exports.createCar = createCar;
+exports.getCars = getCars;
+exports.editCar = editCar;
+exports.deleteCar = deleteCar;
