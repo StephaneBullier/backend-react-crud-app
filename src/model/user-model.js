c7332001599ae2f2c2e08f6c2a088b2b067bd689
Stéphane Bullier <sbullier@gmail.com>
2022-08-01 10:00:11 +0200

    Add validator for email and trim option

--- src/model/user-model.js
+++ src/model/user-model.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = mongoose.Schema({
-  lastname: { type: String, require: true },
-  firstname: { type: String, require: true },
-  email: { type: String, require: true },
-  password: 'password',
-  userType: { type: String, require: true },
-  isActivated: { type: Boolean },
-});
+const userSchema = new mongoose.Schema(
+  {
+    lastname: { type: String, require: true, trim: true },
+    firstname: { type: String, require: true, trim: true },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid');
+        }
+      },
+    },
+    password: { type: String, require: true, trim: true, minLength: 7 },
+    userType: {
+      type: String,
+      enum: ['Administrateur', 'Commercial'],
+      require: true,
+    },
+    isActivated: { type: Boolean },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model('User', userSchema);
 
