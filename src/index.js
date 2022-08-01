e558a2e060d15c61736475beb0aa475dbb22f887
Stéphane Bullier <sbullier@gmail.com>
2022-08-01 09:59:03 +0200

    Add setHeader and userRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ const express = require('express');
 
 require('./db/mongoose');
 
+const userRouter = require('./router/user-router');
 const carRouter = require('./router/car-router');
 
 const app = express();
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use(express.json());
 
+app.use(userRouter);
 app.use(carRouter);
 
 app.listen(port, () => {
